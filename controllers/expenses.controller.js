b54be9eaaf1d9924f0063d671c896293306a153d
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-03-31 14:03:14 +0400

    delete route created

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -18,3 +18,25 @@ exports.make = (req, res) => {
     console.log(err);
   });
 }
+
+exports.delete = (req, res) => {
+  const {id} = req.params;
+
+  expenses.destroy({
+    where: {id}
+  })
+  .then(affected => {
+    if(affected > 0) {
+      res.send({
+        answer: `Expense with id:${id} was successfully deleted!`
+      });
+    } else {
+      res.status(404);
+      res.send({answer: `Expense with id:${id} wasn't found!`});
+    }
+  })
+  .catch(err => {
+    res.status(422).send({answer: "Invalid params"});
+    console.log(err);
+  });
+}
--- routes.js
+++ routes.js
@@ -3,7 +3,10 @@ module.exports = app => {
   const router = require('express').Router();
 
   router.route('/')
-  .post(expensesController.make);
+  .post(expensesController.make)
+  
+  router.route('/:id')
+  .delete(expensesController.delete)
   
   app.use('/api/expenses', router);
 }
