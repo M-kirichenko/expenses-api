65ac8ecd46a4289be837e4b0315fc11b66594f14
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-04-05 21:01:48 +0400

    id-check-added

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -22,6 +22,8 @@ exports.make = (req, res) => {
 exports.delete = (req, res) => {
   const { id } = req.params;
 
+  if(!(id.trim())) res.status(422).send( {answer: "incorrect id!"} );
+
   expenses.destroy({
     where: { id }
   })
@@ -44,37 +46,44 @@ exports.update = (req, res) => {
   const { id } = req.params;
   const { body } = req;
   const { name, date, price } = body;
-  let cols = {};
-
-  if(name) cols = {name};
-
-  if(date) cols = {...cols, date};
-
-  if(price) cols = {...cols, price};
-
+  
+  if(!(id.trim())) res.status(422).send( {answer: "incorrect id!"} );
+  
   if(
-    !cols.hasOwnProperty("name") && 
-    !cols.hasOwnProperty("date") &&
-    !cols.hasOwnProperty("price")
+    !body.hasOwnProperty("name") && 
+    !body.hasOwnProperty("date") &&
+    !body.hasOwnProperty("price")
     ) {
     res.status(422).send( {answer: "No params were send!"} );
   } else {
-    expenses.update(
-      cols,
-      { where: { id } }
-    )
-    .then(affected => {
-      if(affected > 0) {
-          res.send( {answer: `Expense with id:${id} was successfully updated!`} );
-      } else {
-        res.status(404).send({
-          answer: `Expense with id:${id} wasn't found!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(422).send( {answer: "Invalid params"} );
-    });
+    let cols = {};
+
+    if(name) cols = {name};
+  
+    if(date) cols = {...cols, date};
+  
+    if(price) cols = {...cols, price};
+
+    if(Object.keys(cols).length){
+      expenses.update(
+        cols,
+        { where: { id } }
+      )
+      .then(affected => {
+        if(affected > 0) {
+            res.send( {answer: `Expense with id:${id} was successfully updated!`} );
+        } else {
+          res.status(404).send({
+            answer: `Expense with id:${id} wasn't found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(422).send( {answer: "Invalid params"} );
+      });
+    } else {
+      res.send( {answer: `None of params could be empty!`} );
+    }
   }
 }
 
